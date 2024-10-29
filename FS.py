accbbf9db5ae4ad2bf0fde52e1e785f358b16099
RayanIsMe <87342981+RayanIsMe@users.noreply.github.com>
2024-10-29 16:25:26 +0530

    Update FS.py

--- FS.py
+++ FS.py
@@ -31,6 +31,7 @@ initspeed = 0.7 #initial speed the particle travels at
 numberofparticlesnew = 6 #number of particles that are added per time frame
 energylose = 0.7 #amount of energy lost when colliding with edge
 repulsiveDistance = 0.5
+repulsiveStrength = 0.05
 
 #PLOTTING----------------------------------------------
 pltx = []
@@ -47,47 +48,71 @@ for h in range(64):
     ob.append(object(10+(h/8), 5-(h/20), 0, 0))
     objCount += 2
 
-while True: #LOOP STARTS|||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
+while True:  # LOOP STARTS
+    # SETTING VARIABLES AND LIST TO 0
+    pltx = []
+    plty = []
+    deleteList = []
 
-  #SETTING VARIABLES AND LIST TO 0
-  pltx = []
-  plty = []
-  deleteList = []
+    time += 1  # add one to time variable every time we go through the loop
 
-  time += 1 #add one to time variable every time we go through the loop
+    # Add particles every increment of this condition. If initial speed is higher,
+    # more frequent new particles to simulate constant density
+    if time % int(2 / initspeed) == 0:
+        for i in range(numberofparticlesnew):
+            # create a new particle with initial x velocity and initial random vertical position
+            p.append(particle(0, random.randint(0, By - 1) + random.random(), initspeed, 0))
+            particleCount += 1
 
-  if time%int(2/initspeed) == 0: #add particles every increment of this condition. If initial speed is higher, more frequent new particles to simulate constant density
-    for i in range(numberofparticlesnew):
-      p.append(particle(0, random.randint(0,By-1)+random.random(), initspeed, 0)) #create a new particle with initial x velocity and initial random vertical postition.
-      particleCount += 1
+    # LOOPING PARTICLES-------------------------------------------------------------------------
+    for i in range(particleCount):
 
-#LOOPING PARTICLES-------------------------------------------------------------------------
-for i in range(particleCount):
+        # Border collision
+        if p[i].y < 0:
+            p[i].y = 0
+            p[i].Vy *= -energylose
+        elif p[i].y > By:
+            p[i].y = By
+            p[i].Vy *= -energylose
 
-  #boarder collision
-  if p[i].y < 0:
-      p[i].y = 0
-      p[i].Vy *= -energylose
-  elif p[i].y > By:
-      p[i].y = By
-      p[i].Vy *= -energylose
+        if p[i].x < 0:
+            p[i].x = 0
+            p[i].Vx *= -energylose
+        elif p[i].x > Bx:
+            deleteList.append(i)
 
-  if p[i].x < 0:
-      p[i].x = 0
-      p[i].Vx *= -energylose
-  elif p[i].x > Bx:
-    deleteList.append(i)
+        # REPULSIVE FORCE FROM OTHER PARTICLES
+        for j in range(particleCount):
+            if i != j:
+                # Distance variable stores numerical value of distance between two particles
+                distance = ((p[i].x - p[j].x) ** 2 + (p[i].y - p[j].y) ** 2) ** 0.5
+                if distance < repulsiveDistance:
+                    # Find the angle
+                    if p[j].x - p[i].x != 0:
+                        angle = math.atan((p[j].y - p[i].y) / (p[j].x - p[i].x))
+                    else:
+                        angle = 90
 
-  #REPULSIVE FORCE FROM OTHER PARTICLES
-  for j in range(particleCount):
-    if i != j:
-      distance = ((p[i].x - p[j].x)**2 + (p[i].y - p[j].y)**2)**0.5 #distance varaible stores numerical value of distance between two particles
-      if distance < replusiveDistance:
-          #find the angle
-                if ob[j].x - p[i].x != 0:
-                        angle = math.atan((ob[j].y - p[i].y)/(ob[j].x - p[i].x))
-                else:
-                    angle = 90
+                    # Change velocity of particle[i]
+                    if p[i].x <= p[j].x:
+                        p[i].Vx += -math.cos(angle) * (repulsiveDistance / distance) * repulsiveStrength
+                        p[i].Vy += -math.sin(angle) * (repulsiveDistance / distance) * repulsiveStrength
+                    elif p[i].x >= p[j].x:
+                        p[i].Vx += math.cos(angle) * (repulsiveDistance / distance) * repulsiveStrength
+                        p[i].Vy += math.sin(angle) * (repulsiveDistance / distance) * repulsiveStrength
+
+            # UPDATE POSITIONS
+            p[i].x += p[i].Vx
+            p[i].y += p[i].Vy
+
+            #ADD PLOTS
+            pltx.append(p[i].x)
+            plty.append(p[i].y)
+
         
-  
+
+        #DELETE REQUIRED OBJECTS
+        for j in range(len(deleteList)):
+            del p[deleteList[j]]
+            n = n-1
 
