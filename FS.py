d7630252cc9311b0e04ebf8dcafc3fc1a99e267b
RayanIsMe <87342981+RayanIsMe@users.noreply.github.com>
2025-03-19 20:12:52 +0530

    Update FS.py

--- FS.py
+++ FS.py
@@ -4,124 +4,124 @@ import numpy as np
 import streamlit as st
 import time
 
-#CLASS ------------------------------------
-class particle:
-    x, y, Vx, Vy = 0, 0, 0, 0
-    def __init__(self, x, y, Vx, Vy):
-        self.x = x
-        self.y = y
-        self.Vx = Vx
-        self.Vy = Vy
+if 'SS' not in st.session_state:
+    st.session_state['SS'] = 1
 
-class object(particle):
-    def __init__(self, x, y, Vx, Vy):
-        self.x = x
-        self.y = y
-        self.Vx = Vx
-        self.Vy = Vy
+if st.session_state["SS"] == 1:
+    #CLASS ------------------------------------
+    class particle:
+        x, y, Vx, Vy = 0, 0, 0, 0
+        def __init__(self, x, y, Vx, Vy):
+            self.x = x
+            self.y = y
+            self.Vx = Vx
+            self.Vy = Vy
+    
+    class object(particle):
+        def __init__(self, x, y, Vx, Vy):
+            self.x = x
+            self.y = y
+            self.Vx = Vx
+            self.Vy = Vy
+    
+    #VARIABLES-------------------------------------------
+    
+    
+    #defining variables
+    st.session_state["timeP"] = 0 #variable keeping track of number of interations. This variable increases by one every time the program runs through the while loop.
+    st.session_state["particleCount"] = 0 #variable to keep track of number of particles
+    
+    #border
+    st.session_state["Bx"] = 30 #length of area of scatter plot x
+    st.session_state["By"] = 10 #height of area of scatter plot y
+    
+    #control variables
+    st.session_state["initialSpeed"] = 0.7 #initial speed the particle travels at
+    st.session_state["newParticles"] = 6 #number of particles that are added per time frame
+    st.session_state["energyLoss"] = 0.7 #amount of energy lost when colliding with edge
+    st.session_state["repulsiveDistance"] = 0.5
+    st.session_state["repulsiveStrength"] = 0.05
+    
+    #defining particles
+    st.session_state["p"] = [] #object list for air particle objects
+    st.session_state["ob"] = [] #object list for object particle objects
+    
+    #OBJECT DEFINITION-----------------------------------------------------------------------------------------------
+    st.session_state["objCount"] = 0 #keeps track of number of object particles. Stays constant
+    for h in range(64):
+        st.session_state["ob"].append(object(10+(h/32), 5+(h/16), 0, 0))
+        st.session_state["ob"].append(object(10+(h/8), 5-(h/20), 0, 0))
+        objCount += 2
+    
+    st.session_state['SS'] = 2
 
-#VARIABLES-------------------------------------------
-
-#streamlit
-placeholder = st.empty()
-
-#defining variables
-timeP = 0 #variable keeping track of number of interations. This variable increases by one every time the program runs through the while loop.
-particleCount = 0 #variable to keep track of number of particles
-
-#border
-Bx = 30 #length of area of scatter plot x
-By = 10 #height of area of scatter plot y
-
-#control variables
-initspeed = 0.7 #initial speed the particle travels at
-numberofparticlesnew = 6 #number of particles that are added per time frame
-energylose = 0.7 #amount of energy lost when colliding with edge
-repulsiveDistance = 0.5
-repulsiveStrength = 0.05
-
-#PLOTTING----------------------------------------------
-pltx = []
-plty = []
-
-#defining particles
-p = [] #object list for air particle objects
-ob = [] #object list for object particle objects
-
-#OBJECT DEFINITION-----------------------------------------------------------------------------------------------
-objCount = 0 #keeps track of number of object particles. Stays constant
-for h in range(64):
-    ob.append(object(10+(h/32), 5+(h/16), 0, 0))
-    ob.append(object(10+(h/8), 5-(h/20), 0, 0))
-    objCount += 2
-
-while True:  # LOOP STARTS
+elif st.session_state['SS'] == 2:
     # SETTING VARIABLES AND LIST TO 0
     pltx = []
     plty = []
     deleteList = []
 
-    timeP += 1  # add one to time variable every time we go through the loop
+    st.session_state["timeP"] += 1  # add one to time variable every time we go through the loop
 
     # Add particles every increment of this condition. If initial speed is higher,
     # more frequent new particles to simulate constant density
-    if timeP % int(2 / initspeed) == 0:
-        for i in range(numberofparticlesnew):
+    if st.session_state["timeP"] % int(2 / st.session_state["initialSpeed"]) == 0:
+        for i in range(st.session_state["newParticles"]):
             # create a new particle with initial x velocity and initial random vertical position
-            p.append(particle(0, random.randint(0, By - 1) + random.random(), initspeed, 0))
-        particleCount += numberofparticlesnew
+            st.session_state["p"].append(particle(0, random.randint(0, st.session_state["By"] - 1) + random.random(), st.session_state["initialSpeed"], 0))
+        st.session_state["particleCount"] += st.session_state["newParticles"]
 
     # LOOPING PARTICLES-------------------------------------------------------------------------
-    for i in range(particleCount):
+    for i in range(st.session_state["particleCount"]):
 
         # Border collision
-        if p[i].y < 0:
-            p[i].y = 0
-            p[i].Vy *= -energylose
-        elif p[i].y > By:
-            p[i].y = By
-            p[i].Vy *= -energylose
+        if st.session_state["p"][i].y < 0:
+            st.session_state["p"][i].y = 0
+            st.session_state["p"][i].Vy *= -energylose
+        elif st.session_state["p"][i].y > By:
+            st.session_state["p"][i].y = By
+            st.session_state["p"][i].Vy *= -energylose
 
-        if p[i].x < 0:
-            p[i].x = 0
-            p[i].Vx *= -energylose
-        elif p[i].x > Bx:
+        if st.session_state["p"][i].x < 0:
+            st.session_state["p"][i].x = 0
+            st.session_state["p"][i].Vx *= -energylose
+        elif st.session_state["p"][i].x > Bx:
             deleteList.append(i)
 
         # REPULSIVE FORCE FROM OTHER PARTICLES
-        for j in range(particleCount):
+        for j in range(st.session_state["particleCount"]):
             if i != j:
                 # Distance variable stores numerical value of distance between two particles
-                distance = ((p[i].x - p[j].x) ** 2 + (p[i].y - p[j].y) ** 2) ** 0.5
-                if distance < repulsiveDistance:
+                distance = ((st.session_state["p"][i].x - st.session_state["p"][j].x) ** 2 + (st.session_state["p"][i].y - st.session_state["p"][j].y) ** 2) ** 0.5
+                if distance < st.session_state["repulsiveDistance"]:
                     # Find the angle
-                    if p[j].x - p[i].x != 0:
-                        angle = math.atan((p[j].y - p[i].y) / (p[j].x - p[i].x))
+                    if st.session_state["p"][j].x - st.session_state["p"][i].x != 0:
+                        angle = math.atan((st.session_state["p"][j].y - st.session_state["p"][i].y) / (st.session_state["p"][j].x - st.session_state["p"][i].x))
                     else:
                         angle = 90
 
                     # Change velocity of particle[i]
-                    if p[i].x <= p[j].x:
-                        p[i].Vx += -math.cos(angle) * (repulsiveDistance / distance) * repulsiveStrength
-                        p[i].Vy += -math.sin(angle) * (repulsiveDistance / distance) * repulsiveStrength
+                    if st.session_state["p"][i].x <= st.session_state["p"][j].x:
+                        st.session_state["p"][i].Vx += -math.cos(angle) * (st.session_state["repulsiveDistance"] / distance) * st.session_state["repulsiveStrength"]
+                        st.session_state["p"][i].Vy += -math.sin(angle) * (st.session_state["repulsiveDistance"] / distance) * st.session_state["repulsiveStrength"]
                     elif p[i].x >= p[j].x:
-                        p[i].Vx += math.cos(angle) * (repulsiveDistance / distance) * repulsiveStrength
-                        p[i].Vy += math.sin(angle) * (repulsiveDistance / distance) * repulsiveStrength
+                        st.session_state["p"][i].Vx += math.cos(angle) * (st.session_state["repulsiveDistance"] / distance) * st.session_state["repulsiveStrength"]
+                        st.session_state["p"][i].Vy += math.sin(angle) * (st.session_state["repulsiveDistance"] / distance) * st.session_state["repulsiveStrength"]
 
             # UPDATE POSITIONS
-            p[i].x += p[i].Vx
-            p[i].y += p[i].Vy
+            st.session_state["p"][i].x += st.session_state["p"][i].Vx
+            st.session_state["p"][i].y += st.session_state["p"][i].Vy
 
             #ADD PLOTS
-            pltx.append(p[i].x)
-            plty.append(p[i].y)
+            pltx.append(st.session_state["p"][i].x)
+            plty.append(st.session_state["p"][i].y)
 
         
 
         #DELETE REQUIRED OBJECTS
         for j in range(len(deleteList)):
-            del p[deleteList[j]]
-            particleCount -= 1
+            del st.session_state["p"][deleteList[j]]
+            st.session_state["particleCount"] -= 1
 
         #PLOTTING
         pltxy = [pltx, plty]
