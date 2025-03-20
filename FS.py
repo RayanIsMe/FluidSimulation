158efafc6b063ca8964602d65d6cb3dee7b80312
RayanIsMe <87342981+RayanIsMe@users.noreply.github.com>
2025-03-20 15:05:57 +0530

    Update FS.py

--- FS.py
+++ FS.py
@@ -86,7 +86,7 @@ elif st.session_state['SS'] == 2:
 
     # Add particles every increment of this condition. If initial speed is higher,
     # more frequent new particles to simulate constant density
-    if st.session_state["timeP"] % int(2 / st.session_state["initialSpeed"]) == 0:
+    if st.session_state["timeP"] % 5 == 0:   #int(2 / st.session_state["initialSpeed"])
         for i in range(st.session_state["newParticles"]):
             # create a new particle with initial x velocity and initial random vertical position
             st.session_state["p"].append(particle(0, random.randint(0, st.session_state["By"] - 1) + random.random(), st.session_state["initialSpeed"], 0))
@@ -175,6 +175,10 @@ elif st.session_state['SS'] == 2:
         pltx.append(st.session_state["p"][i].x)
         plty.append(st.session_state["p"][i].y)
 
+        st.write(pltx)
+        st.write(plty)
+        st.write("")
+
         
 
         
@@ -202,7 +206,7 @@ elif st.session_state['SS'] == 2:
                         st.session_state['SS'] = 3
                         st.session_state["timeP"] = 0
 
-        st.rerun()
+        #st.rerun()
                 
 elif st.session_state['SS'] == 3:
         if st.session_state["timeP"] <= st.session_state["its"]:
@@ -214,12 +218,12 @@ elif st.session_state['SS'] == 3:
                 
                 with placeholder.container():
                         st.scatter_chart(data = df, x = 'x', y = 'y', width = 700, height = 400)
-                        # for i in range(len(st.session_state["p"])):
-                        #         st.write(st.session_state["p"][i].x)
-                        #         st.write(st.session_state["p"][i].y)
-                        #         st.write("")
-                        st.write(st.session_state["plotx"][st.session_state["timeP"]-1])
-                        st.write( st.session_state["ploty"][st.session_state["timeP"]-1])
+                        for i in range(len(st.session_state["p"])):
+                                st.write(st.session_state["p"][i].x)
+                                st.write(st.session_state["p"][i].y)
+                                st.write("")
+                        # st.write(st.session_state["plotx"][st.session_state["timeP"]-1])
+                        # st.write( st.session_state["ploty"][st.session_state["timeP"]-1])
                 time.sleep(0.1)
                 st.button("Rerun")
         else:
